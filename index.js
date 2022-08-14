c31b4c6320b787f0d59280b12dec3f0ebebaf5cd
Tevin-Mugairwa <tevin.mugairwa@student.moringaschool.com>
2022-08-14 20:49:03 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Feet){
   // Write your code here!
+   if(Feet< 400)
+   {
+      return 'This one is on me!';
+   }
+
+   else if(Feet>2000 && Feet <= 2500 )
+   {
+     return  'I will gladly take your thirty bucks.';
+   }
+
+   else
+   {
+    return 'No can do.';
+   }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city ==='NYC'? 'Ok, sounds good.':`No go.`;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
+  switch(Tip)
+  {
+     case 'generous': 
+           return "Thank you so much.";
+
+     case 'not as generous':
+          return "Thank you.";
+           
+      default:
+        return "Bye.";
+
+
+  }
   // Write your code here!
 }
